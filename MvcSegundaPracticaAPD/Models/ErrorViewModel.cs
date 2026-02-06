cc203f436a2633a49f164db25b4c15bc49db3763
angeldzz <pintodiazangel@gmail.com>
2026-02-06 11:45:54 +0100

    Add project files.

---
+++ MvcSegundaPracticaAPD/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MvcSegundaPracticaAPD.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
